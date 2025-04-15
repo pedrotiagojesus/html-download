3f7c54add338e6465ffa42c0963253eacd84ab93
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2025-04-16 00:31:23 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
     const [previewCss, setPreviewCss] = useState<string>("");
 
     useEffect(() => {
-        console.log(import.meta.env.BASE_URL);
         fetch(`${import.meta.env.BASE_URL}templates/example.html`)
             .then((res) => res.text())
             .then((text) => {
